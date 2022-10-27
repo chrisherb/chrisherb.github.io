2d837f275325e120d1a7e75278e775cbad0f1ce0
Christopher Herb <chrisherb@outlook.com>
2022-10-27 09:59:11 +0200

    using hooks for responsive context

--- src/components/TrnrTitle.tsx
+++ src/components/TrnrTitle.tsx
@@ -1,47 +1,45 @@
 import { StaticImage } from "gatsby-plugin-image";
 import { Box, ResponsiveContext, Heading } from "grommet";
-import React from "react";
+import React, { useContext } from "react";
 
 export function TrnrTitle() {
+  const size = useContext(ResponsiveContext);
+
   return (
-    <ResponsiveContext.Consumer>
-      {(size) => (
-        <Box
-          id="home"
-          align="center"
-          pad={{
-            left: "xlarge",
-            top: "large",
-            right: "xlarge",
-            bottom: "xlarge",
+    <Box
+      id="home"
+      align="center"
+      pad={{
+        left: "xlarge",
+        top: "large",
+        right: "xlarge",
+        bottom: "xlarge",
+      }}
+      background={{ opacity: "medium" }}
+    >
+      <Box
+        width="large"
+        pad="small"
+        margin={{ left: size !== "small" ? "-190px" : "" }}
+      >
+        <Heading level="1" margin="0">
+          <StaticImage
+            src="../images/ternar-music-technology.svg"
+            alt="Ternär Music Technology Logo"
+          />
+        </Heading>
+        <Heading
+          level="2"
+          size="xsmall"
+          margin={{
+            left: size !== "small" ? "165px" : "0",
+            top: "small",
           }}
-          background={{ opacity: "medium" }}
         >
-          <Box
-            width="large"
-            pad="small"
-            margin={{ left: size !== "small" ? "-190px" : "" }}
-          >
-            <Heading level="1" margin="0">
-              <StaticImage
-                src="../images/ternar-music-technology.svg"
-                alt="Ternär Music Technology Logo"
-              />
-            </Heading>
-            <Heading
-              level="2"
-              size="xsmall"
-              margin={{
-                left: size !== "small" ? "165px" : "0",
-                top: "small",
-              }}
-            >
-              Software Instruments and Effects straight from Europe&rsquo;s
-              heart of electronic music, Berlin.
-            </Heading>
-          </Box>
-        </Box>
-      )}
-    </ResponsiveContext.Consumer>
+          Software Instruments and Effects straight from Europe&rsquo;s heart of
+          electronic music, Berlin.
+        </Heading>
+      </Box>
+    </Box>
   );
 }
