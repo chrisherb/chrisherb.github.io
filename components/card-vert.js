552b594595d9b0438300f081e79fa2224dfdd7de
Chris <chris@ternar.tech>
2025-01-28 22:40:35 +0100

    open cards in dialog, listen to window parameters

--- components/card-vert.js
+++ components/card-vert.js
@@ -1,14 +1,21 @@
 class TrnrCardVert extends HTMLElement {
+
   constructor() {
     super();
     this.render();
   }
 
+  id = Date.now();
+
   render() {
     const title = this.getAttribute('title');
     const text = this.getAttribute('text');
     const imgSrc = this.getAttribute('img-src');
-    const id = Date.now();
+
+    // remove content and save it for later
+    const content = this.querySelector('.card-content');
+    if (content)
+      this.removeChild(content);
 
     const template = document.createElement('template');
     template.innerHTML = `
@@ -18,30 +25,75 @@ class TrnrCardVert extends HTMLElement {
           <h5>${title}</h5>
           <p>${text}</p>
           <nav>
-            <button data-ui="#${id}">Buy</button>
+            <button id="open-${this.id}">Buy</button>
           </nav>
         </div>
       </article>    
-      <dialog id="${id}" class="max">
+      <dialog id="dialog-${this.id}" class="max">
         <div class="row">
-          <button class="circle transparent" data-ui="#dialog">
+          <button id="close-${this.id}" class="circle transparent">
             <i>arrow_back</i>
           </button>
           <h3>${title}</h3>
         </div>
-        <div>Some text here</div>
+        ${content?.innerHTML}
         <nav class="right-align">
           <button class="border">Cancel</button>
           <button>Confirm</button>
         </nav>
       </dialog>
     `;
-
     this.appendChild(template.content);
   }
 
-  onDialogOpenClick() {
-    document.querySelector('#dialog').show();
+  connectedCallback() {
+    const dialog = this.querySelector(`#dialog-${this.id}`);
+    const id = this.getAttribute("id");
+
+    const onUrlChange = () => {
+      const urlParams = new URLSearchParams(window.location.search);
+      const paramValue = urlParams.get('page');
+
+      if (paramValue === id) {
+        debugger;
+        dialog.show();
+      } else {
+        dialog.close();
+      }
+    }
+
+    // Listen for popstate events to react to URL changes (back, forward, pushState, replaceState)
+    window.addEventListener('popstate', onUrlChange.bind(this));
+
+    // check for window parameters
+    const urlParams = new URLSearchParams(window.location.search);
+    const paramValue = urlParams.get('page');
+    if (paramValue === id)
+      dialog.show();
+
+    const openBtn = this.querySelector('#open-' + this.id);
+    openBtn.addEventListener("click", () => {
+      this.addQueryParam("page", id);
+      dialog.show();
+    });
+
+    const closeBtn = this.querySelector('#close-' + this.id);
+    closeBtn.addEventListener("click", () => {
+      dialog.close();
+      this.removeQueryParam('page');
+    });
+  }
+
+  removeQueryParam(param) {
+    const url = new URL(window.location.href);
+    url.searchParams.delete(param); // Delete the parameter from the URL
+    window.history.pushState({}, '', url);
+  }
+
+  addQueryParam(param, value) {
+    const url = new URL(window.location.href); // Get current URL
+    url.searchParams.set(param, value); // Add or update the parameter
+    window.history.pushState({}, '', url);
   }
 }
 
