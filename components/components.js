4d137db46807a5af8402e777daf5dda1237b7a40
Chris <chris@ternar.tech>
2025-02-01 12:31:50 +0100

    custom table element

--- components/components.js
+++ components/components.js
@@ -2,4 +2,5 @@ import './header.js';
 import './card.js';
 import './card-vert.js';
 import './list-item.js';
-import './audio-player.js'
+import './audio-player.js';
+import './table-row.js';
---
+++ components/table-row.js
@@ -0,0 +1,48 @@
+class TrnrTable extends HTMLTableElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const tableRows = this.querySelectorAll('tr');
+
+    tableRows.forEach(this.enhanceCells);
+  }
+
+  enhanceCells(row) {
+    const tableCells = row.querySelectorAll('td');
+
+    tableCells.forEach((cell, cellIndex) => {
+      switch (cellIndex) {
+        case 0:
+          var innerText = cell.innerText;
+          var wrapper = '<strong>' + innerText + '</strong>';
+          cell.innerHTML = wrapper;
+          break;
+        case 1:
+          var innerText = cell.innerText;
+          var wrapper = '<em>' + innerText + '</em>';
+          cell.innerHTML = wrapper;
+          break;
+        case 2:
+          cell.classList.add('min')
+          cell.querySelectorAll('button').forEach((button, btnIndex) => {
+            button.classList.add('chip', 'circle');
+            const btnText = button.innerText;
+            
+            switch(btnIndex) {
+              case 0:
+                button.innerHTML = `<i>window</i><div class="tooltip left">${btnText}</div>`;
+              break;
+              case 1:
+                button.innerHTML = `<i>nutrition</i><div class="tooltip left">${btnText}</div>`;
+              break;
+            }
+          });
+          break;
+      }
+    })
+  }
+}
+
+window.customElements.define('trnr-table', TrnrTable, { extends: 'table' });
