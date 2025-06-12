1aa9900e89ac82c48b5c43c541d8029d268e22e1
Chris <chris@ternar.tech>
2025-06-12 20:02:36 +0200

    sticky header component

--- components/header.js
+++ components/header.js
@@ -5,15 +5,22 @@ class TrnrHeader extends HTMLElement {
   }
 
   render() {
-    const template = document.createElement('template');
+    const template = document.createElement("template");
     template.innerHTML = `
       <header class="fixed responsive max surface-container">
         <nav class="scroll">
-          <button class="square transparent" onclick="location.href='#';">
-            <img class="responsive" src="/logo.svg" style="opacity: 0.7" />
-          </button>
-          <nav class="center-align max no-space">
-            <button class="border left-round" onclick="location.href='#software';">
+          <a href="#">
+            <img
+              class="responsive"
+              src="/logo.svg"
+              style="opacity: 0.9; width: 40px; height: 40px"
+            />
+          </a>
+          <nav class="group connected center-align max">
+            <button
+              class="border left-round"
+              onclick="location.href='#software';"
+            >
               <span>Software</span>
             </button>
             <button class="border no-round" onclick="location.href='#music';">
@@ -22,7 +29,10 @@ class TrnrHeader extends HTMLElement {
             <button class="border no-round" onclick="location.href='#about';">
               <span>About</span>
             </button>
-            <button class="border right-round" onclick="location.href='#contact';">
+            <button
+              class="border right-round"
+              onclick="location.href='#contact';"
+            >
               <span>Contact</span>
             </button>
           </nav>
@@ -32,11 +42,6 @@ class TrnrHeader extends HTMLElement {
 
     this.appendChild(template.content);
   }
-
-  getBtnClass(id) {
-    const selected = this.getAttribute('selected') || 0;
-    return `${selected == id ? 'fill' : ''}`;
-  }
 }
 
-window.customElements.define('trnr-header', TrnrHeader);
+window.customElements.define("trnr-header", TrnrHeader);
